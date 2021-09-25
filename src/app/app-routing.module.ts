cb9668bb510a9a48f36641565fe0be2991b900da
rkcamnv <camnv@rikkeisoft.com>
2021-09-25 13:43:57 +0700

    create module customers and products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: '', pathMatch: 'full', redirectTo: '/customers' },
+  { path: 'customers', loadChildren: () => import('./pages/customers/customers.module').then(m => m.CustomersModule) },
+  { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) }
 ];
 
 @NgModule({
